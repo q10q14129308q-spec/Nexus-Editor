6cf040fd1e603009df83bc38119c292f8fe16e7b
q10q14129308q-spec <q10q14129308q@gmail.com>
2026-07-28 22:24:17 +0800

    feat(paste): add packages/plugin-paste/src/index.ts

---
+++ packages/plugin-paste/src/index.ts
@@ -0,0 +1,12 @@
+export {
+  createPastePlugin,
+  type PastePlugin,
+  type PastePluginOptions
+} from "./plugin";
+
+export {
+  convertHtmlToMarkdown,
+  createConverter,
+  type PasteConvertOptions,
+  type TurndownRule
+} from "./convert";
